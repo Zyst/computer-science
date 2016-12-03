69ea3f70ceb4dbf0f86c202195c736c1e0c9a838
Zyst <zyst@gmx.com>
2016-12-02 21:38:15 -0600

    Go fmt

--- cs50/pset1/water/water.go
+++ cs50/pset1/water/water.go
@@ -13,14 +13,14 @@ func main() {
 	// Thanks http://stackoverflow.com/a/3751456/1224232
 	var minutes int
 
-  // While structure in Go. While minutes is less than 1...
+	// While structure in Go. While minutes is less than 1...
 	for minutes < 1 {
 		fmt.Printf("minutes: ")
 
-    // We pass a pointer of our minutes variable for assignment
+		// We pass a pointer of our minutes variable for assignment
 		_, err := fmt.Scanf("%d", &minutes)
 
-    // If we have an error, which covers almost anything, we keep looping
+		// If we have an error, which covers almost anything, we keep looping
 		if err != nil {
 			fmt.Println("We need a positive number")
 		}
