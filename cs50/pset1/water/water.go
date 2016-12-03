0a834138ea862f775c5bc46a814da0853e12cdd3
Zyst <zyst@gmx.com>
2016-12-02 21:19:31 -0600

    Error handling for Water works!

--- cs50/pset1/water/water.go
+++ cs50/pset1/water/water.go
@@ -5,20 +5,20 @@ import (
 )
 
 func minutesToWaterBottles(min int) int {
-  return min * 192 / 16
+	return min * 192 / 16
 }
 
 func main() {
-  // Thanks http://stackoverflow.com/a/3751456/1224232
-  var minutes int
+	// Thanks http://stackoverflow.com/a/3751456/1224232
+	var minutes int
 
-  fmt.Printf("minutes: ")
-  _, err := fmt.Scanf("%d", &minutes)
+	for minutes < 1 {
+		fmt.Printf("minutes: ")
+		_, err := fmt.Scanf("%d", &minutes)
+		if err != nil {
+			fmt.Println("We need a positive number")
+		}
+	}
 
-  // We should add error (Loop) handling for minutes input, make sure # is positive, and a number
-  if err != nil {
-    panic(err)
-  }
-
-  fmt.Printf("bottles: %d\n", minutesToWaterBottles(minutes))
+	fmt.Printf("bottles: %d\n", minutesToWaterBottles(minutes))
 }
