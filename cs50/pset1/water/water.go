00566a2a12946b24ff5d5dc9d7e4f7e0437528aa
Zyst <zyst@gmx.com>
2016-12-02 21:32:41 -0600

    Documentation for Water

--- cs50/pset1/water/water.go
+++ cs50/pset1/water/water.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Function to translate our minutes to "Water Bottles"
 func minutesToWaterBottles(min int) int {
 	return min * 192 / 16
 }
@@ -12,9 +13,14 @@ func main() {
 	// Thanks http://stackoverflow.com/a/3751456/1224232
 	var minutes int
 
+  // While structure in Go. While minutes is less than 1...
 	for minutes < 1 {
 		fmt.Printf("minutes: ")
+
+    // We pass a pointer of our minutes variable for assignment
 		_, err := fmt.Scanf("%d", &minutes)
+
+    // If we have an error, which covers almost anything, we keep looping
 		if err != nil {
 			fmt.Println("We need a positive number")
 		}
