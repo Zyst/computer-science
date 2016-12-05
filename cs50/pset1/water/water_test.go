6081232672fdb39a091ab35f6118c8adabb98262
Zyst <zyst@gmx.com>
2016-12-04 22:53:32 -0600

    Wrote tests for minutes to water bottles

---
+++ cs50/pset1/water/water_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func Test_minutesToWaterBottles(t *testing.T) {
+	type args struct {
+		min int
+	}
+	tests := []struct {
+		name    string
+		minutes int
+		want    int
+	}{
+		// TODO: Add test cases.
+		{"120", 10, 120},
+		{"1200", 100, 1200},
+		{"180", 15, 180},
+		{"3996", 333, 3996},
+		{"12000", 1001, 12000},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minutesToWaterBottles(tt.minutes); got != tt.want {
+				t.Errorf("minutesToWaterBottles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
