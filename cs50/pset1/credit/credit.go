7995b65e4cf1712c1b07d2da1f37daf8f778fdf8
Zyst <zyst@gmx.com>
2016-12-06 21:40:24 -0600

    Check input is numbers function for credit

--- cs50/pset1/credit/credit.go
+++ cs50/pset1/credit/credit.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 // Our first test, we are only checking
 // American Express, Mastercard, and Visa.
+// We check that the passed card has a length of
+// 13 characters, 15 characters, or 16 characters.
 func checkCardLengthValidity(card string) bool {
   // If our length is 13, 15, or 16 we have passed the first test
   return len(card) == 13 || len(card) == 15 || len(card) == 16
+}
+
+// We make sure everything we input is a number.
+// If someone types 378282246310005abcd that's invalid.
+func checkInputIsNumbers(card string) bool {
+  _, err := strconv.Atoi(card)
+
+  // If we have no trouble converting Atoi we should be safe.
+  if err != nil {
+    return false
+  }
+  return true
 }
 
 func main() {
@@ -19,8 +34,8 @@ func main() {
   test := "378282246310005"
 
   // If our card doesn't pass length tests we return
-  if !checkCardLengthValidity(test) {
-    fmt.Println("INVALID\n")
+  if !checkCardLengthValidity(test) && !checkInputIsNumbers(test) {
+    fmt.Println("INVALID")
     return
   }
 }
