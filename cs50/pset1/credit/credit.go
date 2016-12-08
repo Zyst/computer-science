bde481300a478bbd240e67a09b04167e248c55be
Zyst <zyst@gmx.com>
2016-12-07 19:43:30 -0600

    first sum completed

--- cs50/pset1/credit/credit.go
+++ cs50/pset1/credit/credit.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 // Our first test, we are only checking
@@ -10,32 +11,89 @@ import (
 // We check that the passed card has a length of
 // 13 characters, 15 characters, or 16 characters.
 func checkCardLengthValidity(card string) bool {
-  // If our length is 13, 15, or 16 we have passed the first test
-  return len(card) == 13 || len(card) == 15 || len(card) == 16
+	// If our length is 13, 15, or 16 we have passed the first test
+	return len(card) == 13 || len(card) == 15 || len(card) == 16
 }
 
 // We make sure everything we input is a number.
 // If someone types 378282246310005abcd that's invalid.
 func checkInputIsNumbers(card string) bool {
-  _, err := strconv.Atoi(card)
+	_, err := strconv.Atoi(card)
 
-  // If we have no trouble converting Atoi we should be safe.
-  if err != nil {
-    return false
-  }
-  return true
+	// If we have no trouble converting Atoi we should be safe.
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func verifyCard(card string) bool {
+	// We split our card into individual numbers
+	numbers := strings.Split(card, "")
+
+	// Our first sum following the algorithm
+	firstSum := 0
+
+	// Our second sum following the algorithm
+	secondSum := 0
+
+	for index, num := range numbers {
+		// If it's an alternating number
+		if index%2 != 0 {
+			i, err := strconv.Atoi(num)
+
+			if err != nil {
+				panic(err)
+			}
+
+			sum := i * 2
+
+			// If sum is less than 10
+			if sum < 10 {
+				firstSum += sum
+			} else {
+				// We turn our multiplied sum back to a string
+        sumString := strconv.Itoa(sum)
+
+        // We split the string into parts (Should just be two)
+        split := strings.Split(sumString, "")
+
+        // We convert the first element back to an int
+        x, err := strconv.Atoi(split[0])
+
+        if err != nil {
+          panic(err)
+        }
+
+        // We convert our second element back to an int
+        y, err := strconv.Atoi(split[1])
+
+        if err != nil {
+          panic(err)
+        }
+
+        // We add X and Y to our first sum
+        firstSum = firstSum + x + y
+			}
+		}
+	}
+
+  fmt.Println(firstSum)
+	return true
 }
 
 func main() {
-  // Here are some credit cards for testing!
-  // https://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
+	// Here are some credit cards for testing!
+	// https://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
 
-  // TODO: Add User input here
-  test := "378282246310005"
+	// TODO: Add User input here
+	test := "378282246310005"
 
-  // If our card doesn't pass length tests we return
-  if !checkCardLengthValidity(test) && !checkInputIsNumbers(test) {
-    fmt.Println("INVALID")
-    return
-  }
+	// If our card doesn't pass length tests we return
+	if !checkCardLengthValidity(test) && !checkInputIsNumbers(test) {
+		fmt.Println("INVALID")
+		return
+	}
+
+	verifyCard(test)
 }
