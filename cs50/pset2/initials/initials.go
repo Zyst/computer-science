0022d06835b53d66316d6030d39b2acf729f6614
Zyst <zyst@gmx.com>
2016-12-15 23:06:02 -0600

    Initial main function for user input

--- cs50/pset2/initials/initials.go
+++ cs50/pset2/initials/initials.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -14,12 +17,29 @@ func getInitials(fullname string) string {
 	result := ""
 
 	for _, name := range names {
-    result += string(name[0])
+		result += string(name[0])
 	}
 
 	return strings.ToUpper(result)
 }
 
 func main() {
+	var name string
 
+	// While our string is less than 1 char long
+	for len(name) < 1 {
+		fmt.Println("What's your name?")
+
+		reader := bufio.NewReader(os.Stdin)
+
+		input, _, err := reader.ReadLine()
+
+		if err != nil {
+			panic(err)
+		}
+
+    name = string(input)
+	}
+
+  fmt.Printf("Your initials are %s\n", getInitials(name))
 }
