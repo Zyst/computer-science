23cd8fc1e9169371676fc2828be7201178e358db
Zyst <zyst@gmx.com>
2017-01-14 00:02:30 -0600

    formatting

--- cs50/pset2/vigenere/vigenere.c
+++ cs50/pset2/vigenere/vigenere.c
@@ -2,100 +2,119 @@
 #include <stdlib.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
 
 char vigenereShift(char, char);
 void vigenereCipher(string, string);
 bool isUpper(char);
 bool isLower(char);
+bool checkKeyAsAlpha(string);
 
 int main(int argc, string argv[])
 {
-  // If we don't have exactly two arguments
-  if (argc != 2)
-  {
-    printf("Usage: ./vigenere k\n");
-    return 1;
-  }
+    // If we don't have exactly two arguments
+    if (argc != 2 || !checkKeyAsAlpha(argv[1]))
+    {
+        printf("Usage: ./vigenere k\n");
+        return 1;
+    }
 
-  // Our first argument is the key we'll use for encryption
-  string key = argv[1];
+    // Our first argument is the key we'll use for encryption
+    string key = argv[1];
 
-  // We receive a string from the user to encrypt
-  printf("plaintext:  ");
-  string encrypt = GetString();
+    // We receive a string from the user to encrypt
+    printf("plaintext:  ");
+    string encrypt = GetString();
 
-  vigenereCipher(encrypt, key);
+    vigenereCipher(encrypt, key);
 
-  return 0;
+    return 0;
+}
+
+// Checks if a string has exclusively alphabetical characters
+bool checkKeyAsAlpha(string key)
+{
+    // We start with a true check
+    bool alphaCheck = true;
+
+    for (int i = 0, n = strlen(key); i < n; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            alphaCheck = false;
+        }
+    }
+
+    return alphaCheck;
 }
 
 bool isUpper(char letter)
 {
-  return letter >= 'A' && letter <= 'Z';
+    return letter >= 'A' && letter <= 'Z';
 }
 
 bool isLower(char letter)
 {
-  return letter >= 'a' && letter <= 'z';
+    return letter >= 'a' && letter <= 'z';
 }
 
 // Does the Vigenere cipher letter shift
 char vigenereShift(char letter, char offset)
 {
-  if (isUpper(letter))
-  {
-
-    // If our offset is lowercase we turn it into uppercase
-    // because Vigenere keys are case insensitive
-    if (isLower(offset))
+    if (isUpper(letter))
     {
-      offset -= 32;
-    }
 
-    // We return the letter turned into an alphabetical index (0-25 A-Z) plus
-    // the offset turned into an alphabetical index, then we do a modulo
-    // 26 operation, and finally turn the letter into the ASCII range again
-    return ((letter - 'A' + offset - 'A') % 26) + 'A';
-  }
-  else if (isLower(letter))
-  {
-    // If our offset is uppercase we turn it into lowercase
-    // because Vigenere keys are case insensitive
-    if (isUpper(offset))
+        // If our offset is lowercase we turn it into uppercase
+        // because Vigenere keys are case insensitive
+        if (isLower(offset))
+        {
+            offset -= 32;
+        }
+
+        // We return the letter turned into an alphabetical index (0-25 A-Z) plus
+        // the offset turned into an alphabetical index, then we do a modulo
+        // 26 operation, and finally turn the letter into the ASCII range again
+        return ((letter - 'A' + offset - 'A') % 26) + 'A';
+    }
+    else if (isLower(letter))
     {
-      offset += 32;
-    }
+        // If our offset is uppercase we turn it into lowercase
+        // because Vigenere keys are case insensitive
+        if (isUpper(offset))
+        {
+            offset += 32;
+        }
 
-    // We return the letter turned into an alphabetical index (0-25 a-z) plus
-    // the offset turned into an alphabetical index, then we do a modulo
-    // 26 operation, and finally turn the letter into the ASCII range again
-    return ((letter - 'a' + offset - 'a') % 26) + 'a';
-  }
-  // If we got to this point we return the character unchanged
-  return letter;
+        // We return the letter turned into an alphabetical index (0-25 a-z) plus
+        // the offset turned into an alphabetical index, then we do a modulo
+        // 26 operation, and finally turn the letter into the ASCII range again
+        return ((letter - 'a' + offset - 'a') % 26) + 'a';
+    }
+    // If we got to this point we return the character unchanged
+    return letter;
 }
 
 // We iterate through our string, running our vignere shift algorithm on each
 // individual letter, and printing it out
 void vigenereCipher(string encrypt, string key)
 {
-  // A subindex I'll use for the Vigenere key
-  int subIndex = 0;
-  int keyLength = strlen(key);
+    // A subindex I'll use for the Vigenere key
+    int subIndex = 0;
+    int keyLength = strlen(key);
 
-  printf("ciphertext: ");
+    printf("ciphertext: ");
 
-  for (int i = 0, n = strlen(encrypt); i < n; i++)
-  {
-    printf("%c", vigenereShift(encrypt[i], key[subIndex % keyLength]));
-
-    if (isUpper(encrypt[i]) || isLower(encrypt[i]))
+    for (int i = 0, n = strlen(encrypt); i < n; i++)
     {
-      subIndex++;
-    }
-  }
+        printf("%c", vigenereShift(encrypt[i], key[subIndex % keyLength]));
 
-  // Newline to finish our call
-  printf("\n");
+        if (isUpper(encrypt[i]) || isLower(encrypt[i]))
+        {
+            subIndex++;
+        }
+    }
+
+    // Newline to finish our call
+    printf("\n");
 }
