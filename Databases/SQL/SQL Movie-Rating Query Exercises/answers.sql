2cfc718294e8209818501d0a5b37a7efb0543a03
Zyst <erick.romero.dev@gmail.com>
2017-08-04 14:34:43 -0500

    Q6

--- Databases/SQL/SQL Movie-Rating Query Exercises/answers.sql
+++ Databases/SQL/SQL Movie-Rating Query Exercises/answers.sql
@@ -25,4 +25,7 @@ from Movie, Reviewer, Rating using (mID, rID)
 order by name, title, stars
 
 -- Q6 For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie.
-
+-- I'm like 100% sure there must be a way more elegant way to do this
+select name, title
+from Rating as R1, Rating as R2, Reviewer using (rID), Movie using (mID)
+where R1.rID = R2.rID and R1.mID = R2.mID and R1.stars > R2.stars and R1.ratingDate > R2.ratingDate
