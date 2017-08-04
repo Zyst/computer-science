c5cfe07682631e043a87049bb205f5a529c3b87f
Zyst <erick.romero.dev@gmail.com>
2017-08-04 11:53:39 -0500

    Q1 and Q2

---
+++ Databases/SQL/SQL Movie-Rating Query Exercises/answers.sql
@@ -0,0 +1,10 @@
+-- Q1 Find the titles of all movies directed by Steven Spielberg.
+select title
+from Movie
+where director = "Steven Spielberg"
+
+-- Q2 Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order
+select distinct year
+from Rating, Movie using(mID)
+where stars = 5 or stars = 4
+order by year
