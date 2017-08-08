dddd33b17574a7bbaf5303b047b5d772e20bf240
Zyst <erick.romero.dev@gmail.com>
2017-08-08 12:28:50 -0500

    Progress on Q5

--- Databases/SQL/SQL Movie-Rating Query Exercises Extras/answers.sql
+++ Databases/SQL/SQL Movie-Rating Query Exercises Extras/answers.sql
@@ -22,3 +22,8 @@ where Movie.mID not in (
   select mID
   from Rating, Reviewer using (rID)
   where name = "Chris Jackson")
+
+-- Q5 For all pairs of reviewers such that both reviewers gave a rating to the same movie, return the names of both reviewers. Eliminate duplicates, don't pair reviewers with themselves, and include each pair only once. For each pair, return the names in the pair in alphabetical order.
+select name, rID, mID
+from Rating, Reviewer using (rID)
+order by mID
