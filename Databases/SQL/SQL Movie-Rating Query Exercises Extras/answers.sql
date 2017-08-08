27ed7435a3d8c7af751c104ca8eaf923c10552fb
Zyst <erick.romero.dev@gmail.com>
2017-08-08 11:47:10 -0500

    Q2 and Q3

--- Databases/SQL/SQL Movie-Rating Query Exercises Extras/answers.sql
+++ Databases/SQL/SQL Movie-Rating Query Exercises Extras/answers.sql
@@ -2,3 +2,15 @@
 select distinct name
 from Rating, Reviewer using (rID), Movie using (mID)
 where title = "Gone with the Wind"
+
+-- Q2 For any rating where the reviewer is the same as the director of the movie, return the reviewer name, movie title, and number of stars.
+select name, title, stars
+from Rating, Reviewer using (rID), Movie using (mID)
+where director = name
+
+-- Q3 Return all reviewer names and movie names together in a single list, alphabetized. (Sorting by the first name of the reviewer and first word in the title is fine; no need for special processing on last names or removing "The".)
+select name
+from Reviewer
+union
+select title
+from Movie
